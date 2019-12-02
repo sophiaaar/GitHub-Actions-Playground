d8c7af67346d0cc2aa10fd89376586c29891db1f
sophia <sophia@unity3d.com>
2019-12-02 16:50:41 +0100

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -1,9 +1,11 @@
 import subprocess
+import sys
+import argv
 
 print("testing scripting")
 
-git_branch = subprocess.check_output(['git', 'branch']).strip()
-print(git_branch)
+#git_branch = subprocess.check_output(['git', 'branch']).strip()
+print('git_branch:' + sys.argv[1])
 
 # data = '''{
   # "source": {
