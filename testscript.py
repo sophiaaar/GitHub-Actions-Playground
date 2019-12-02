2a3c781de681dba1fa3bf11fcb4f1391a6dbb9fd
sophia <sophia@unity3d.com>
2019-12-02 17:03:11 +0100

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -3,19 +3,4 @@ import sys
 
 print("testing scripting")
 
-#git_branch = subprocess.check_output(['git', 'branch']).strip()
 print('git_branch:' + sys.argv[1])
-
-# data = '''{
-  # "source": {
-    # "branchname": "master",
-    # "revision": "''' + srp_revision + '''"
-  # },
-  # "links": {
-    # "project": "/projects/78",
-    # "jobDefinition": "/projects/78/revisions/''' + srp_revision + '''/job-definitions/.yamato#upm-ci-abv.yml#trunk_verification"
-  # },
-  # "environmentVariables": [
-    # { "key": "CUSTOM_REVISION", "value": "''' + unity_revision + '''" }
-# ]
-# }'''
